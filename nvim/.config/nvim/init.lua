5c5947bb11fe26ffefdb17e1e9b915c5d57a75d7
Dominic Marshall <dom@marshall34.co.uk>
2024-10-08 21:52:02 +0100

    fork init

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,15 +1,31 @@
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
-  })
-end
-vim.opt.rtp:prepend(lazypath)
+-- Initialize packer
+require('packer').startup(function(use)
+  -- Packer can manage itself
+  use 'wbthomason/packer.nvim'
 
-require("vim-options")
-require("lazy").setup("plugins")
+  -- One Dark Pro theme
+  use 'olimorris/onedarkpro.nvim'
+
+  -- Telescope for file navigation (CMD + P functionality)
+  use {
+    'nvim-telescope/telescope.nvim', tag = '0.1.8',
+    requires = { {'nvim-lua/plenary.nvim'} }
+  }
+
+  -- Optionally add other plugins here
+end)
+
+-- Enable true color support for One Dark Pro
+vim.o.termguicolors = true
+
+-- Apply the One Dark Pro theme
+vim.cmd([[colorscheme onedark]])
+
+-- Enable line numbers globally
+vim.o.number = true
+vim.o.relativenumber = true
+
+-- Map Ctrl + P to Telescope's file finder using Lua function
+local builtin = require('telescope.builtin')
+vim.api.nvim_set_keymap('n', '<C-p>', '', { callback = builtin.find_files, noremap = true, silent = true })
+
